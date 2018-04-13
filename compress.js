6b9ca0d6f1964643c5271fc0191b9f90bfa5ebfc
hanik <estyle81@gmail.com>
2018-04-13 21:01:32 +0900

    commit

--- compress.js
+++ compress.js
@@ -1,21 +1,49 @@
-const fs = require('fs');
-const zip = new require('node-zip')();
+const fs = require('fs')
+const zip = new require('node-zip')()
+const luxon = require('luxon')
 
-const removeFileNames = []
-const dir = fs.readdirSync('./datas')
-dir.forEach((fileName) => {
-    if (fileName.endsWith('.txt')) {
-        const path = `./datas/${fileName}`
-        zip.file(fileName, fs.readFileSync(path))
-        removeFileNames.push(path)
+const run = () => {
+    const weekday = luxon.DateTime.local().weekday
+    if(weekday != 5) {
+        console.log(`This is not Friday.`)
+        return;
     }
-})
 
-const data = zip.generate({ base64:false, compression: 'DEFLATE' });
+    console.log(`Friday compressing!`)
+    const utc = luxon.DateTime.local().toFormat('yyLLdd_HH')
+    const today = luxon.DateTime.local().toFormat('yyLLdd')
+    const PATH = './datas'
+    const removeFileNames = []
+    const dir = fs.readdirSync(PATH)
 
-// it's important to use *binary* encode
-fs.writeFileSync('test.zip', data, 'binary');
+    console.log(`Prepare files...`)
+    dir.forEach((fileName, index) => {
+        if(fileName.endsWith('.txt') && fileName.indexOf(utc) < 1){
+            const path = `${PATH}/${fileName}`
+            zip.file(fileName, fs.readFileSync(path))
+            removeFileNames.push(path)
+        }
+    })
 
-removeFileNames.forEach( (path) => {
-    fs.unlinkSync(path)
-})
+    console.log(`Generate zip file...`)
+    let zipFileName = `./compress/datas_${dir[0].split('_')[1]}_${today}.zip`
+    console.log(zipFileName)
+
+    const data = zip.generate({ base64:false, compression: 'DEFLATE' });
+
+    console.log(`Writeing zip file...`)
+    // it's important to use *binary* encode
+    fs.writeFileSync(zipFileName, data, 'binary');
+
+    console.log(`Remove compressed files.`)
+    removeFileNames.forEach( (filepath) => {
+        fs.unlinkSync(filepath)
+    })
+
+    console.log(`Compress done.`)
+}
+
+module.exports = {
+    run,
+}
+
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // Include the server in your file
-const server = require('server');
+const server = require('server')
 const grepper = require('./grepper')
-const { get, post } = server.router;
+const compress = require('./compress')
+const { get } = server.router
 
 // Handle requests to the url "/" ( http://localhost:3000/ )
 server([
     get('/', ctx => 'Hello world!')
-]);
+])
 
-let times = 1;
-let seq = 0;
+let times = 1
+let seq = 0
 setInterval(() => {
     seq++
-if(seq === 60) {
-    console.log(`Grepped ${60 * times} datas...`)
-    seq = 0
-    times++
-}
-grepper.grap('ALL')
-}, 1000);
+    if(seq === 60) {
+        console.log(`Grepped ${60 * times} datas...`)
+        seq = 0
+        times++
+    }
+    grepper.grap('ALL')
+}, 1000)
+
+setInterval(compress.run, 1000 * 60 * 60 * 24)
